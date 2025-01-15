5f300c83d4c5901df5fda6144d17419b3e3a4b87
sirihelenew <sirihwa@stud.ntnu.no>
2025-01-15 11:16:56 +0100

    Pythonscript for plot fra CSV-filer

---
+++ README.md
@@ -0,0 +1,21 @@
+# Plotting av måleresultater med Python 🧚🏼‍♀️
+
+Denne guiden viser hvordan du kan lese inn og plotte måleresultater fra en CSV-fil ved hjelp av Python. Skriptet er laget for ESDA-fagene og tar deg steg for steg gjennom hvordan du lager figurer med `matplotlib`.
+
+**NB! Husk å lagre .csv-filene i samme mappe som pythonscriptet.**
+
+
+## Forutsetninger 🛠️
+
+Du må ha følgende Python-pakker installert:
+- `numpy`
+- `matplotlib`
+
+Installer dem ved å skrive:
+```bash
+pip install numpy matplotlib
+```
+
+
+### Matplotlib-dokumentasjon
+[Matplotlib Pyplot Summary](https://matplotlib.org/stable/api/pyplot_summary.html)
--- plot.py
+++ plot.py
@@ -1,33 +1,61 @@
-import csv
+import numpy as np
+#Plotting
 import matplotlib.pyplot as plt
-
+#Lese CSV
+import csv
 
 header = []
 data = []
+filename = '0alAMP-kopi.csv'
 
-filename = "d3_22.csv"
-
+#Henter data fra csvfil
 with open(filename) as csvfile:
     csvreader = csv.reader(csvfile)
-
+#Leser første linje i csv-fila (den med navn til kanalene)
     header = next(csvreader)
-
     for datapoint in csvreader:
-
         values = [float(value) for value in datapoint]
         data.append(values)
 
-print(header)
-print(data[0])
-print(data[1])
+#Legger inn data fra hver kanal i hver sin liste
+time = [(p[0]) for p in data]
+ch1 = [(p[1]) for p in data]
+ch2 = [(p[2]) for p in data]
 
-time = [p[0] for p in data]
-ch1 = [p[1] for p in data]
-ch2 = [p[2] for p in data] 
 
-plt.ylabel("Amplitude [V]")
-plt.xlabel("Tid [s]")
-plt.plot(time,ch1)
+#----- Enkelt plot ------
+plt.plot(time, ch1)
 plt.plot(time, ch2)
-plt.legend(["x_1(t)", "x_k(t)"])
-plt.show()
+plt.savefig('filnavn.png', dpi=500) #Lagrer figuren, endre filnavn
+#plt.show() #Viser figuren
+
+
+
+#----- Subplots ------
+fig, (channel1, channel2) = plt.subplots(2,1) #2 rader, 1 kolonne
+#fig.suptitle('Tittel')
+channel1.plot(time, ch1) #Bruk labels med legend()
+channel2.plot(time, ch2)
+
+plt.savefig('filnavn2.png', dpi=500)
+
+
+#----- Titler ++ ------
+fig, (ax) = plt.subplots(1,1) #1 rad, 1 kolonne
+ax.plot(time,ch1, label = 'ch1') # Bruk labels med legend()
+ax.plot(time,ch2, label = 'ch2', color='hotpink', ) #Endre farge
+ax.legend(loc='upper right')
+
+ax.set_title('Oscilloskop')
+ax.set_xlabel('Tid (s)')
+ax.set_ylabel('Spenning (V)')
+
+ax.grid(True) #Rutenett
+
+#ax.set_xlim(-0.02,0.02) #Endrer utsnitt
+
+plt.savefig('filnavn3.png', dpi=500)
+#show the fig
+#plt.show()
+
+
